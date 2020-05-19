17d61c08910f35a3442fd547b49b8ab363840d2f
cballest <cballest@pros.com>
2020-05-19 17:49:58 -0500

    search ovas

--- service/ovas-service.js
+++ service/ovas-service.js
@@ -33,12 +33,12 @@ function getAllOvas(req, res, next) {
 
 function searchOVA(req, res, next) {
     let searchString = req.params.query;
-    dbInstance.any('SELECT * FROM public.\"OVAS\" WHERE '+
-        '"TITLE" ILIKE \'%$1%\' OR' +
-        '"SUBJECT" ILIKE \'%$1%\' OR' +
-        '"DESCRIPTION" ILIKE \'%$1%\' OR' +
-        '"ALTERNATIVE" ILIKE \'%$1%\' OR' +
-        '"EDUCATION_LEVEL" ILIKE \'%$1%\'', searchString)
+    dbInstance.any('SELECT * FROM public.\"OVAS\" WHERE ' +
+        '"TITLE" ILIKE \'%$1#%\' OR' +
+        '"SUBJECT" ILIKE \'%$1#%\' OR' +
+        '"DESCRIPTION" ILIKE \'%$1#%\' OR' +
+        '"ALTERNATIVE" ILIKE \'%$1#%\' OR' +
+        '"EDUCATION_LEVEL" ILIKE \'%$1#%\'', searchString)
         .then(data => {
             res.status(200)
                 .json({

634dbacb41f2379dbb3fbe89b25e1a1505301c2f
cballest <cballest@pros.com>
2020-05-19 15:28:47 -0500

    json db

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/db', async (req, res) => {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM test_table');
     const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
+    res.status(200).json({data: results});
     client.release();
   } catch (err) {
     console.error(err);
